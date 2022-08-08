429258ce01f6655fbf59329b39eb73a8ae9c62f5
Tyler Jackson <tylerjamesjackson@outlook.com>
2022-08-08 19:51:52 -0400

    feat: add ability to check and score answers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,137 @@
-import React from "react"
+import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage';
-import GamePage from "./components/GamePage";
+import GamePage from './components/GamePage';
+import uuid from 'react-uuid';
 
 function App() {
-
-  function changePage(){
-    setCurrentPage('gamePage')
+  function changePage() {
+    setCurrentPage((prevPage) => {
+      if (prevPage === 'landingPage') {
+        return 'gamePage';
+      } else if (prevPage === 'gamePage') {
+        return 'checkPage';
+      }
+    });
   }
-  
-  const [currentPage, setCurrentPage] = React.useState('landingPage')
 
-  const displayPage = currentPage === 'landingPage' ? <LandingPage changePage={changePage}/> : <GamePage />
+  //Set state variable to hold array
+  const [questions, setQuestions] = React.useState([]);
+
+  function formatQuestions(questions) {
+    return questions.map((question) => {
+      const incorrectArray = question.incorrect_answers;
+      incorrectArray.push(question.correct_answer);
+      return {
+        id: uuid(),
+        question: question.question,
+        answers: incorrectArray.map((answer, index) => {
+          return {
+            id: uuid(),
+            answer: answer,
+            correct: incorrectArray.length - 1 === index ? true : false,
+            selected: false,
+          };
+        }),
+      };
+    });
+  }
+
+  function shuffleArray(questions) {
+    const unshuffled = formatQuestions(questions);
+    const shuffled = unshuffled.map((question) => {
+      return {
+        ...question,
+        answers: question.answers.sort(() => {
+          return Math.random() - 0.5;
+        }),
+      };
+    });
+    return shuffled;
+  }
+
+  const [playAgain, setPlayAgain] = React.useState(0);
+
+  function playGameAgain() {
+    setPlayAgain((prev) => prev + 1);
+    setCurrentPage('gamePage');
+    setCorrectAnswers({
+      num_correct: 0,
+      total_questions: 0,
+    });
+  }
+
+  //Get array of questions
+  React.useEffect(() => {
+    fetch('https://opentdb.com/api.php?amount=5')
+      .then((res) => res.json())
+      .then((data) => shuffleArray(data.results))
+      .then((output) => setQuestions(output));
+  }, [playAgain]);
+
+  function selectAnswer(event, questionID, answerID) {
+    setQuestions((prev) =>
+      prev.map((question) => {
+        if (question.id === questionID) {
+          return {
+            ...question,
+            answers: question.answers.map((answer) => {
+              if (answer.id === answerID) {
+                return { ...answer, selected: !answer.selected };
+              } else {
+                return { ...answer, selected: false };
+              }
+            }),
+          };
+        } else {
+          return question;
+        }
+      })
+    );
+  }
+
+  const [currentPage, setCurrentPage] = React.useState('landingPage');
+
+  const [correctAnswers, setCorrectAnswers] = React.useState({
+    num_correct: 0,
+    total_questions: 0,
+  });
+
+  function gradeQuiz() {
+    setCorrectAnswers((prev) => {
+      return { ...prev, total_questions: questions.length };
+    });
+    questions.forEach((question) => {
+      question.answers.forEach((answer) => {
+        if (answer.correct && answer.selected) {
+          setCorrectAnswers((prev) => {
+            return {
+              ...prev,
+              num_correct: (prev.num_correct += 1),
+            };
+          });
+        }
+      });
+    });
+    changePage();
+  }
+
+  let displayPage;
+  if (currentPage === 'landingPage') {
+    displayPage = <LandingPage changePage={changePage} />;
+  } else {
+    displayPage = (
+      <GamePage
+        questions={questions}
+        selectAnswer={selectAnswer}
+        changePage={changePage}
+        currentPage={currentPage}
+        playGameAgain={playGameAgain}
+        gradeQuiz={gradeQuiz}
+        correctAnswers={correctAnswers}
+      />
+    );
+  }
 
   return (
     <main>
@@ -19,7 +139,6 @@ function App() {
       {displayPage}
       <img src="blob-b.png" alt="blob-b" className="blob-b"></img>
     </main>
-   
   );
 }
 
