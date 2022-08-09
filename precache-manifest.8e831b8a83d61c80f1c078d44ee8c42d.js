ef032527048566e4e0d2ab40a35be08b45b4a5ad
Tyler Jackson <tylerjamesjackson@outlook.com>
2022-08-08 20:03:22 -0400

    Updates

---
+++ precache-manifest.8e831b8a83d61c80f1c078d44ee8c42d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36dacfc4f34eaafa00ae",
+    "url": "/quizzical/static/css/main.bf836ec8.chunk.css"
+  },
+  {
+    "revision": "36dacfc4f34eaafa00ae",
+    "url": "/quizzical/static/js/main.36dacfc4.chunk.js"
+  },
+  {
+    "revision": "eb80e1d32435b0877428",
+    "url": "/quizzical/static/js/1.eb80e1d3.chunk.js"
+  },
+  {
+    "revision": "10828be3562dbc652641",
+    "url": "/quizzical/static/js/runtime~main.10828be3.js"
+  },
+  {
+    "revision": "2609ad7c4ff002820c8fadc8dbc2f369",
+    "url": "/quizzical/index.html"
+  }
+];
